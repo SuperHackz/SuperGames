75c68a00298e7e765648439cddbf46e2d735b0ea
SuperHackz <79522273+SuperHackz@users.noreply.github.com>
2025-03-04 16:40:45 +0000

    Update subtitlequotes.js

--- scripts/inject/subtitlequotes.js
+++ scripts/inject/subtitlequotes.js
@@ -1,24 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const quotes = [
-        "Better than any school, even you Labay.",
-        "thank you X-88 for saving my boredom - some random student ",
-        "SCOTT HOW ARE YOU STILL ALIVE?! - blooket haters",
+        "Better than any school, even you Hasmo.",
+        "thank you Games for saving my boredom - some random student ",
+        "DAN HOW ARE YOU STILL ALIVE?! - blooket haters",
         "Thank you for checking out our website!!",
         "wild fact: scott spends years on a google site, it's still up..",
-        "We update almost DAILY! Expect atleast a new game every new day.",
-        "wild fact: Opium was planned to be the next cybriagames, then corrupted LOL",
-        "wild fact: X-88 redid this website, it was worth it.",
-        "wild fact: Opium was infamous for actually getting featured on a schoolwide slideshow video.. wow.",
+        "We update almost DAILY! Expect at least a new game every new day.",
+        "wild fact: Dan redid this website, it was worth it.",
+        "wild fact: Games was infamous for actually getting featured on a schoolwide slideshow video.. wow.",
         "Deployed on Firebase AKA web.app by SC0TT!",
         "if you read this you need to lose your IQ immediately.",
-        "The best uprising game website, mainly for CFISD.",
-        "Potato! - SC0TT",
-        "Suggest a new game to add on the widgetbot server or https://discord.gg/c3AmKk67vt",
-        "Submit a custom splash to the widgetbot server!",
-        "Fanny Fact: CybriaGames was published on my birthday",
-        "Fun Faact: CybriaGames was supposed to be an extension of Tam's Game Hub",
-        "does this actually work?",
-        "oh wait NVM it's not that automatic lol"
+        "The best uprising game website, mainly for Hasmo.",
+        "Fanny Fact: Games was published on my birthday",
+        "Fun Fact: Games was supposed to be an extension of Dan's Game Hub",
     ];
 
     const subtitleElement = document.querySelector('.subtitle');
